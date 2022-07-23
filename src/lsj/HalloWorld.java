a5ff78ced4a8ae1c4a6e8534532802389a2ca3b3
lsj <2804587922@qq.com>
2022-07-23 22:28:26 +0800

    55

---
+++ src/lsj/HalloWorld.java
@@ -0,0 +1,11 @@
+package src.lsj;
+
+/**
+ * @author lsj
+ * @create 2022/7/23  21:04
+ */
+public class HalloWorld {
+    public static void main(String[] args) {
+        System.out.println("77777777777777777");
+    }
+}
